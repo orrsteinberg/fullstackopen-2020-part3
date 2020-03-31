11de59e9c7c369781c4677da04163735df470b1e
Orr-J-Sun <orr.sun10@gmail.com>
2020-03-31 20:08:21 +0300

    added prod build for the frontend and a procfile

---
+++ build/precache-manifest.37867306f6409a7c0d260d40a728b0c8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ad75c7fcca9832ad558e9d211c2f03f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1b64e86cc9d720d09967",
+    "url": "/static/css/main.e1b0b02f.chunk.css"
+  },
+  {
+    "revision": "fdc00f3d2f4001d77518",
+    "url": "/static/js/2.9d147208.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.9d147208.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1b64e86cc9d720d09967",
+    "url": "/static/js/main.1332bce1.chunk.js"
+  },
+  {
+    "revision": "9d2b4f7262278411ffe7",
+    "url": "/static/js/runtime-main.4464e187.js"
+  }
+]);
